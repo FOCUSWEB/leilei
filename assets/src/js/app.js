5585e7788b20b0570469ae7a2a93c12951947924
FOCUS <964590970@qq.com>
2015-11-16 22:21:32 +0800

    updated

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -2,23 +2,34 @@
  * Created by focus on 2015/11/16.
  */
 "use strict";
-var app = angular.module('app', ['ui.router']);
+var app = angular.module('app', ['ngAnimate', 'ui.router']);
 
 app.config(function ($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/');
+	$urlRouterProvider.otherwise("/home");
 
-    $stateProvider
-        .state('frontend', {
-            url: '/',
-            templateUrl: "index.html"
-        })
-        .state('frontend.home', {
-            url: "/home",
-            templateUrl: "templates/frontend.html"
-        })
-        .state('frontend.price', {
-            url: "/price",
-            templateUrl: "templates/partials/price.html"
-        })
-    ;
+	$stateProvider
+		.state('frontend', {
+			url: "/",
+			templateUrl: "templates/frontend.html"
+		})
+		.state('frontend.home', {
+			url: "home",
+			views: {
+				"content": {
+					templateUrl: "templates/partials/home.html"
+				}
+			}
+		})
+		.state('frontend.price', {
+			url: "price",
+			views: {
+				"content": {
+					templateUrl: "templates/partials/price.html"
+				}
+			}
+		})
+		.state("login", {
+			url: "/login",
+			templateUrl: "templates/login.html"
+		});
 });
--- assets/src/js/controllers.js
+++ assets/src/js/controllers.js
@@ -1,5 +1,5 @@
 /**
  * Created by focus on 2015/11/16.
  */
-app.controller('GlobalCtrl',function($scope){
+app.controller('MainCtrl',function($scope){
 });
